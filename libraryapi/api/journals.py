a34e1592ee50c3507bc00e3f39831c1f05d1cf31
John Boamah <joboa2015@gmail.com>
2022-12-25 08:43:17 -0500

    Add db models for Journal

--- libraryapi/api/journals.py
+++ libraryapi/api/journals.py
@@ -3,9 +3,12 @@ Journals api
 """
 from flask import Blueprint, request, jsonify
 
+from libraryapi.models import Journal
+
 journals = Blueprint("journals", __name__, url_prefix="/api/v1/")
 
 
 @journals.get('/journals/')
 def fetch_journals():
-    return jsonify({"journals": "journal 1"})
+    journals = Journal.query.all()
+    return jsonify([journal.to_dict() for journal in journals])
--- libraryapi/models.py
+++ libraryapi/models.py
@@ -1,15 +1,73 @@
+"""
+Data classes for the libraryapi application
+"""
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 
 class Book(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def to_dict(self):
+        return dict(id=self.id,
+                    title=self.title,
+                    created_at=self.created_at.strftime('%Y-%m-%d %H:%M:%S'))
 
 
-# class Journal(db.Model):
-#   pass
+class Journal(db.Model):
+    __tablename__ = 'journals'
 
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+    publisher = db.Column(db.String(500), nullable=False)
+    publication_date = db.Column(db.String(100))
+    first_author = db.Column(db.String(255), nullable=False)
+    second_author = db.Column(db.String(255))
+    last_author = db.Column(db.String(255))
+    authors_shorten = db.Column(db.Text)
+    url_to_journal = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def get_last_name(self, args):
+        if args:
+            return args.split()[-1].capitalize()
+        return ""
+
+    def generate_authors(self):
+        self.first_author = self.get_last_name(self.first_author)
+        self.second_author = self.get_last_name(self.second_author)
+        self.last_author = self.get_last_name(self.last_author)
+
+        if self.first_author != "" and self.second_author != "" and self.last_author != "":
+            self.authors_shorten = self.first_author + " " + "et" + " .al"
+        elif self.first_author != "" and self.second_author != "":
+            self.authors_shorten = self.first_author + " and " + self.second_author
+        else:
+            self.authors_shorten = self.first_author
+        return self.authors_shorten
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.authors_shorten = self.generate_authors()
+
+    def to_dict(self):
+        return dict(id=self.id,
+                    title=self.title,
+                    publisher=self.publisher,
+                    publication_date=self.publication_date,
+                    first_author=self.first_author,
+                    second_author=self.second_author,
+                    last_author=self.last_author,
+                    authors_shorten=self.authors_shorten,
+                    url_to_journal=self.url_to_journal,
+                    created_at=self.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+                    updated_at=self.created_at.strftime('%Y-%m-%d %H:%M:%S'))
 
 # class Admin(db.Model):
 #   pass

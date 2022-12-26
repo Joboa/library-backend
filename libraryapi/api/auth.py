9c341927dc1911ef7114cfc2a063e6fb60dca195
John Boamah <joboa2015@gmail.com>
2022-12-26 00:22:57 -0500

    Update database models

--- libraryapi/api/auth.py
+++ libraryapi/api/auth.py
@@ -2,14 +2,20 @@
 """
 Users registration and authentication
 """
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+
+from libraryapi.models import db, User
 
 auth = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
 
 
 @auth.post('/register/')
 def register():
-    return "User created"
+    data = request.get_json()
+    user = User(**data)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.to_dict()), 201
 
 
 @auth.get('/me/')
--- libraryapi/models.py
+++ libraryapi/models.py
@@ -2,21 +2,80 @@
 Data classes for the libraryapi application
 """
 from datetime import datetime
+import validators
+from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
+
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 
+class User(db.Model):
+    # __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    books = db.relationship('Book', backref="user")
+    journals = db.relationship('Journal', backref="user")
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
+
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password = generate_password_hash(password, method='sha256')
+
+    @classmethod
+    def authenticate(cls, **kwargs):
+        username = kwargs.get('username')
+        email = kwargs.get('email')
+        password = kwargs.get('password')
+
+        # validations
+        if not email or not password:
+            return jsonify({"error": "Username and Password are required"})
+
+        if len(password) < 6:
+            return jsonify({"error": "Password is too short"}), 400
+
+        if len(username) < 3:
+            return jsonify({"error": "Username is too short"}), 400
+
+        if not validators.email(email):
+            return jsonify({"error": "Email is not valid"}), 400
+
+        user = cls.query.filter_by(email=email).first()
+        if not user or not check_password_hash(user.password, password):
+            return None
+
+        return user
+
+    def to_dict(self):
+        return dict(id=self.id, username=self.username, email=self.email)
+
+
 class Book(db.Model):
     __tablename__ = 'books'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
+    edition = db.Column(db.String(255))
+    book_type = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
 
     def to_dict(self):
         return dict(id=self.id,
                     title=self.title,
-                    created_at=self.created_at.strftime('%Y-%m-%d %H:%M:%S'))
+                    edition=self.edition,
+                    book_type=self.book_type,
+                    user_id=self.user_id,
+                    created_at=self.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+                    updated_at=self.created_at.strftime('%Y-%m-%d %H:%M:%S'))
 
 
 class Journal(db.Model):
@@ -31,8 +90,9 @@ class Journal(db.Model):
     last_author = db.Column(db.String(255))
     authors_shorten = db.Column(db.Text)
     url_to_journal = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
 
     def get_last_name(self, args):
         if args:
@@ -40,16 +100,16 @@ class Journal(db.Model):
         return ""
 
     def generate_authors(self):
-        self.first_author = self.get_last_name(self.first_author)
-        self.second_author = self.get_last_name(self.second_author)
-        self.last_author = self.get_last_name(self.last_author)
+        f_author = self.get_last_name(self.first_author)
+        s_author = self.get_last_name(self.second_author)
+        l_author = self.get_last_name(self.last_author)
 
-        if self.first_author != "" and self.second_author != "" and self.last_author != "":
-            self.authors_shorten = self.first_author + " " + "et" + " .al"
-        elif self.first_author != "" and self.second_author != "":
-            self.authors_shorten = self.first_author + " and " + self.second_author
+        if f_author != "" and s_author != "" and l_author != "":
+            self.authors_shorten = f_author + " " + "et" + " .al"
+        elif f_author != "" and s_author != "":
+            self.authors_shorten = f_author + " and " + s_author
         else:
-            self.authors_shorten = self.first_author
+            self.authors_shorten = f_author
         return self.authors_shorten
 
     def __init__(self, **kwargs):
@@ -66,12 +126,6 @@ class Journal(db.Model):
                     last_author=self.last_author,
                     authors_shorten=self.authors_shorten,
                     url_to_journal=self.url_to_journal,
+                    user_id=self.user_id,
                     created_at=self.created_at.strftime('%Y-%m-%d %H:%M:%S'),
                     updated_at=self.created_at.strftime('%Y-%m-%d %H:%M:%S'))
-
-# class Admin(db.Model):
-#   pass
-
-
-# class User(db.Model):
-#   pass

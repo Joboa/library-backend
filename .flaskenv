7b5668fd14bc165dff9c56e50a88fa67e35166f6
John Boamah <joboa2015@gmail.com>
2023-01-19 20:44:41 -0500

    Add deployment settings for heroku

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,1 @@
-FLASK_APP=runserver
+FLASK_APP=app
--- libraryapi/config.py
+++ libraryapi/config.py
@@ -10,7 +10,8 @@ load_dotenv()
 
 class BaseConfig(object):
     FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get(
         'SQLALCHEMY_TRACK_MODIFICATIONS')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')

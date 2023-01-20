f504c763cfe0e1be5e02ac84f28a1ac621c32c56
John Boamah <joboa2015@gmail.com>
2023-01-19 21:11:35 -0500

    Update server

--- runserver.py
+++ runserver.py
@@ -4,7 +4,7 @@ creates an application instance and runs the dev server
 
 from libraryapi.application import create_app
 
+app = create_app()
+
 if __name__ == '__main__':
-    
-    app = create_app()
     app.run()
